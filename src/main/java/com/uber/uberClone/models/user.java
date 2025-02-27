51f8f262c568a1da2e1a426994d9c2bf476e63fc
protolabzshekhar <protolabzshekhar@gmail.com>
2025-02-27 17:38:33 +0530

    integrated mongoDB

---
+++ src/main/java/com/uber/uberClone/models/user.java
@@ -0,0 +1,9 @@
+package com.uber.uberClone.models;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = 'users')
+public class user {
+private String id;
+private String name
+}
